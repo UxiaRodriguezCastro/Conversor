03f89a75cd3100b125b2fccd2ee23f29090ecf51
unknown <gelucho09@gmail.com>
2019-05-16 10:33:56 +0200

    Refactorización_Lonxitude

--- src/lonxitude/Clase_Lonxitude.java
+++ src/lonxitude/Clase_Lonxitude.java
@@ -9,38 +9,51 @@ public class Clase_Lonxitude {
     }   
         public void Lonxituderesultado (int r){
             Scanner leer=new Scanner (System.in);
-            double kms, Millas, Pulgadas, RES;
+            double kms, metros, cms, pulgadas, yardas, pies, millas, millas_nauticas, RES;
             int OP=r;
+            //Opción 1
             switch (OP){
-            case 1:
-                System.out.println( "Kms a Millas" );
-                System.out.println( "Ingrese los kms" );
-                kms = leer.nextDouble();
-                RES = kms / 1.609;
-                System.out.println( "El resultado es: "+RES+" Millas" );
-                break;
-            case 2:
-                System.out.println( "Millas a kms" );
-                System.out.println( "Ingrese las Millas" );
-                Millas = leer.nextDouble();
-                RES = Millas * 1.609;
-                System.out.println( "El resultado es: "+RES+" kms " );
-                break;
-            case 3:
-                System.out.println( "Millas a Pulgadas" );
-                System.out.println( "Ingrese las Pulgadas" );
-                Millas = leer.nextDouble();
-                RES = Millas / 1.609;
-                System.out.println( "El resultado es: "+RES+" Pulgas" );
-                break;
-            case 4:
-                System.out.println( "Pulgadas a Millas" );
-                System.out.println( "Ingrese las Pulgadas" );
-                Pulgadas = leer.nextInt();
-                RES = Pulgadas * 1.609;
-                System.out.println( "El resultado es: "+RES+" Millas " );
-                break;
+                //Hay que hacer un switch dentro de cada opción con los 7 casos posibles
+                case 1:
+                    System.out.println( "Kilómetros" );
+                    kms = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+                case 2:
+                    System.out.println( "Metros" );
+                    metros = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+                case 3:
+                    System.out.println( "Centímetros" );
+                    cms = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+                case 4:
+                    System.out.println( "Pulgadas" );
+                    pulgadas = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+                case 5:
+                    System.out.println( "Yardas" );
+                    yardas = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+                case 6:
+                    System.out.println( "Piés" );
+                    pies = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+                case 7:
+                    System.out.println( "Millas" );
+                    millas = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+                case 8:
+                    System.out.println( "Millas Náuticas" );
+                    millas_nauticas = leer.nextDouble();
+                    System.out.println( "Introduzca la longitud a convertir" );
+                    break;
+            }
         }
-        }
-
 }
